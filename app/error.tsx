e096638a740fbc865ede06ed8dbf9a99df3ddfa5
Jamestsalikis <j.tsalikis@ganellen.com>
2026-05-14 16:45:36 +1000

    fix: remove Sentry import from error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import { useEffect } from 'react';
-import * as Sentry from '@sentry/nextjs';
-
 export default function Error({
   error,
   reset,
@@ -10,10 +7,6 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
-
   return (
     <div
       style={{
