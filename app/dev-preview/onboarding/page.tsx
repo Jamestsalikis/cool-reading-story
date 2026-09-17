5bd4cf39b2956d020849a6ffa78aa41709fbb432
Jamestsalikis <j.tsalikis@ganellen.com>
2026-09-17 13:46:07 +1000

    Delete app/dev-preview/onboarding/page.tsx

--- app/dev-preview/onboarding/page.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-const OnboardingPage = dynamic(() => import('../../onboarding/page'), { ssr: false });
-export default function Page() { return <OnboardingPage />; }
